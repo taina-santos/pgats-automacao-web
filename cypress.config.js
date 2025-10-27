3ae9bcbd3f7aa37a052845cd46abacc9b688d102
taina-santos <santostainamd@gmail.com>
2025-10-26 21:15:47 -0500

    Adicionando número de retries no cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  retries: {
+    openMode: 0,
+    runMode: 2
+  },
   e2e: {
     reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
